79114076200255635b7c9b183a5c55c86cfd72b5
LandonHarter <landonharter@outlook.com>
2025-09-27 15:07:41 -0400

    backend context

--- backend/index.ts
+++ backend/index.ts
@@ -1,11 +1,25 @@
 import { serve } from "@hono/node-server";
 import dotenv from "dotenv";
+import { auth } from "./lib/auth";
 import { createHono } from "./lib/hono";
-import authRouter from "./routes/auth";
 
 dotenv.config();
 
-const app = createHono().basePath("/api").route("/auth", authRouter);
+const app = createHono().basePath("/api").use("*", async (c, next) => {
+  const session = await auth.api.getSession({
+    headers: c.req.raw.headers,
+  });
+
+  if (!session) {
+    c.set("user", null);
+    c.set("session", null);
+    return next();
+  }
+
+  c.set("user", session.user);
+  c.set("session", session.session);
+  return next();
+});
 
 serve({
   fetch: app.fetch,
